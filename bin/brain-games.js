1b48a1f6d386308af9c562dc1e4b3aa71ac3453c
gtxpit <gtxpit.jr@gmail.com>
2026-03-26 09:38:59 +0700

    step 3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import getName from '../src/cli.js'
 
-function app () {
+export default function app () {
     console.log("Welcome to the Brain Games!")
-    const name = readlineSync.question('May I have your name?');
-    console.log(`Hello, ${name}!`)
+    getName()
 }
 
 app()
